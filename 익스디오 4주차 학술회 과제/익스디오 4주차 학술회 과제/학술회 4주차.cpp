d47ef317e1508a8efa813e2b268ca1faf3e98cb4
leehyunkee <brdn2925@gmail.com>
2022-05-04 21:04:12 +0900

    학술회 4주차 마무리

---
+++ 익스디오 4주차 학술회 과제/익스디오 4주차 학술회 과제/학술회 4주차.cpp
@@ -0,0 +1,34 @@
+#include<stdio.h>
+ 
+
+
+
+int main()
+{
+	/*
+	int num[10] = { 10, 2, 3 };
+
+	for (int i = 0; i < 10; i++)
+	{
+		printf("%d \n", num[i]);
+	}*/
+	
+	/*
+	char str[6] = "hello";
+	printf("%s", &str);
+	*/
+
+	/*
+	int a;
+	scanf_s("%d", &a);
+	
+	char s1[10];
+
+
+	scanf_s("%s", s1, 10);
+	printf("%s\n", s1);
+	*/
+
+	
+	return 0;
+}
